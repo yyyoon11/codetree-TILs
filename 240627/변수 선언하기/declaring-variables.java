fdd8f1ddcb8696bd6497ee22e432e86be88a43e9
yyyoon11 <yj95981102@gmail.com>
2024-06-27 22:52:08 +0900

    Title: 변수 선언하기, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240627/변수 선언하기/declaring-variables.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        String b = "C";
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
