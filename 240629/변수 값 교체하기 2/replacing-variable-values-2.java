faef66e72cc22af64ce039b18dcab814cfdd3b23
yyyoon11 <yj95981102@gmail.com>
2024-06-29 22:51:17 +0900

    Title: 변수 값 교체하기 2, Time: 82ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240629/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        a = b; // a = 3;
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
