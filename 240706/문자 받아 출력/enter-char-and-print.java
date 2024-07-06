5847fd33b3c4794c94b3937a26b46b5ffe1e9ae3
yyyoon11 <yj95981102@gmail.com>
2024-07-06 23:24:17 +0900

    Title: 문자 받아 출력, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

--- 240706/문자 받아 출력/enter-char-and-print.java
+++ 240706/문자 받아 출력/enter-char-and-print.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        // char c = s.chartAt(0);
-        System.out.println(s);
+        char c = sc.next().charAt(0);
+        System.out.println(c);
     }
 }
