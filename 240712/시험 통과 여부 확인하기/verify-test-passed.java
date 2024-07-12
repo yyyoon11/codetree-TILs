f454dacf9b683b511f3d2da9b0e558f30f31bb3f
yyyoon11 <yj95981102@gmail.com>
2024-07-12 23:08:14 +0900

    Title: 시험 통과 여부 확인하기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240712/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n >= 80) {
+            System.out.println("pass");
+        }
+        else {
+            int a = 100 - n;
+            System.out.println(a + " more score");
+        }
+    }
+}
