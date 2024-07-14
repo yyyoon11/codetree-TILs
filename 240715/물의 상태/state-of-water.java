f8afaedf7e27a65cdcbb4a8ea3d0c93c43b44a96
yyyoon11 <yj95981102@gmail.com>
2024-07-15 00:22:20 +0900

    Title: 물의 상태, Time: 160ms, Memory: 10MB, Status: Passed - CodeTree

--- 240715/물의 상태/state-of-water.java
+++ 240715/물의 상태/state-of-water.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n < 100) {
+        if (n < 0) {
             System.out.println("ice");
         }
         else if (n >= 100) {
