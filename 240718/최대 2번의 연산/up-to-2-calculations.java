aa76ec52426a84f1cd631a1bc65e428aee3bd569
yyyoon11 <yj95981102@gmail.com>
2024-07-18 23:53:42 +0900

    Title: 최대 2번의 연산, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240718/최대 2번의 연산/up-to-2-calculations.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 2 == 0) {
+            a = a / 2;
+        }
+        if (a % 2 != 0) {
+            a = (a + 1) / 2;
+        }
+        System.out.println(a);
+    }
+}
